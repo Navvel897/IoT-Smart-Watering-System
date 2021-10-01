bbb3c8677c0871763676d2cf72e3edd5e02dc484
Navvel897 <56282493+Navvel897@users.noreply.github.com>
2021-10-01 09:55:24 +0700

    smart watering system
    this is the main file in the repo

---
+++ SmartWatering.ino
@@ -0,0 +1,26 @@
+int sensorPin = A0;
+int sensorValue;
+int limit = 700;
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(2, OUTPUT); //Digital Pin D4 
+}
+
+void loop()
+{
+  sensorValue = analogRead(sensorPin);
+  Serial.println("Kelembapan : ");
+  Serial.println(sensorValue);
+
+  if (sensorValue>limit)
+  {
+    digitalWrite(2, LOW);
+  }
+  else
+  {
+    digitalWrite(2, HIGH);
+  }
+  delay(1000);
+}
